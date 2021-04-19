b8fbcbf02c2e3a6f3179035c948b4d4e01a45f1e
blowle <dydcjf8889@gmail.com>
2021-04-20 08:28:42 +0900

    Initial Commit

---
+++ AppFlawManageFramework/AppFlawManageFramework.h
@@ -0,0 +1,18 @@
+//
+//  AppFlawManageFramework.h
+//  AppFlawManageFramework
+//
+//  Created by Dev on 2021/04/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppFlawManageFramework.
+FOUNDATION_EXPORT double AppFlawManageFrameworkVersionNumber;
+
+//! Project version string for AppFlawManageFramework.
+FOUNDATION_EXPORT const unsigned char AppFlawManageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFlawManageFramework/PublicHeader.h>
+
+
